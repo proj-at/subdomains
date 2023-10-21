fd634af894f3fd7ded26aeaa5f2671544ee19aff
Jun Y <awa@outlook.ie>
2023-10-21 15:55:06 -0700

    feat(domains): add proj.sbs

---
+++ domains/proj.sbs/@.js
@@ -0,0 +1,18 @@
+// Reserved for Proj.at
+export default {
+    owner: {
+        // your github username
+        user: "proj-at",
+        // your github email
+        email: "github@proj.at",
+    },
+    record: {
+        // type, accept CNAME, A, AAAA
+        type: "CNAME",
+        record: "proj-at.netlify.app.",
+        // using Cloudflare CDN
+        proxied: true,
+        // TTL, (s), must be between 60 and 86400,
+        ttl: 60,
+    }
+}
