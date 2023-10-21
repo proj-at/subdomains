94a377a30b632dc338d169ed7dc307e205771aef
Jun Y <ohhhcky@outlook.com>
2023-10-21 13:09:33 -0700

    feat: add templates

---
+++ domains/template.js
@@ -0,0 +1,13 @@
+// change the file to [sub].js
+export default {
+    owner: {
+        // your github username
+        user: "proj-at",
+        // your github email
+        email: "github@proj.at",
+    },
+    record: {
+        type: "CNAME",
+        record: "cname.vercel-dns.com."
+    }
+}
