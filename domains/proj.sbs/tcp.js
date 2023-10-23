6d61f57212bd738989bb228f8499938307e64688
Jun Y <awa@outlook.ie>
2023-10-22 18:26:50 -0700

    Update tcp.js

--- domains/proj.sbs/tcp.js
+++ domains/proj.sbs/tcp.js
@@ -6,13 +6,15 @@ export default {
         // your github email
         email: "contact.thecipherproject@gmail.com",
     },
-    record: {
-        // type, accept CNAME, A, AAAA
-        type: "CNAME",
-        record: "4d62.github.io",
-        // using Cloudflare CDN
-        proxied: true,
-        // TTL, (s), must be between 60 and 86400
-        ttl: 60,
-    }
+    records: [
+        {
+            // type, accept CNAME, A, AAAA
+            type: "CNAME",
+            record: "4d62.github.io",
+            // using Cloudflare CDN
+            proxied: true,
+            // TTL, (s), must be between 60 and 86400
+            ttl: 60,
+        }
+    ]
 }
