93b03b3b197591cbead31ff6d9905c9d6a8b7115
Jun Y <awa@outlook.ie>
2023-10-22 18:23:01 -0700

    update template.js

--- domains/template.js
+++ domains/template.js
@@ -6,13 +6,16 @@ export default {
         // your github email
         email: "github@proj.at",
     },
-    record: {
-        // type, accept CNAME, A, AAAA
-        type: "CNAME",
-        record: "cname.vercel-dns.com.",
-        // using Cloudflare CDN
-        proxied: true,
-        // TTL, (s), must be between 60 and 86400
-        ttl: 60,
-    }
+    record: [
+        // can include multiple records, if some of them conflict, may be overwritten
+        {
+            // type of DNS record
+            type: "CNAME",
+            record: "cname.vercel-dns.com.",
+            // using Cloudflare CDN
+            proxied: true,
+            // TTL, (s), must be between 60 and 86400
+            ttl: 60,
+        },
+    ]
 }
