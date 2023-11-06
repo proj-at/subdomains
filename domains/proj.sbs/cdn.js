d633466855abda0438f90e37832347c8c7c7b2b8
4d62 <domry@duck.com>
2023-11-06 18:18:16 -0500

    Create cdn.js

---
+++ domains/proj.sbs/cdn.js
@@ -0,0 +1,14 @@
+export default {
+    owner: {
+        user: "4d62",
+        email: "domry@duck.com",
+    },
+    records: [
+        {
+            type: "CNAME",
+            record: "reserved.svr-int.4d62.is-a.dev",
+            proxied: true,
+            ttl: 60,
+        }
+    ]
+}
