df160a42d4adac3446b66137d9b670d1334c42f1
Maxine Walker <53411077+KingMaxine@users.noreply.github.com>
2023-11-19 02:02:18 +0100

    removed cname record

--- domains/proj.sbs/kmbot.js
+++ domains/proj.sbs/kmbot.js
@@ -8,15 +8,6 @@ export default {
 	records: [
 		// can include multiple records, if some of them conflict, may be overwritten
 		{
-			// type of DNS record
-			type: "CNAME",
-			record: "cname.vercel-dns.com.",
-			// using Cloudflare CDN
-			proxied: false,
-			// TTL, (s), must be between 60 and 86400
-			ttl: 60,
-		},
-		{
 			type: "A",
 			record: "193.122.52.120",
 			// using Cloudflare CDN
