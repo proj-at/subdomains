80ee7ccdd8f17a6f88780741436bdd0d501d441f
Atlas 罗昕乐 <121162287+AtlasL1@users.noreply.github.com>
2023-12-05 19:24:04 +0800

    Create cybervault.js

---
+++ domains/proj.sbs/cybervault.js
@@ -0,0 +1,16 @@
+export default {
+    owner: {
+        // your github username
+        user: "AtlasL1",
+        // your github email
+        email: "loxingle1@gmail.com",
+    },
+    records: [
+        {
+            type: "CNAME",
+            record: "https://cybervau1t.github.io/Main-Site/",
+            proxied: false,
+            ttl: 60,
+        }
+    ]
+}
