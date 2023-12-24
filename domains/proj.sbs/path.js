9040b2814550466573ebb2af6e8c77987083b62c
pathfindertogami <pathfindertogami@mail.ru>
2023-12-24 23:03:58 +0500

    Create path.js

---
+++ domains/proj.sbs/path.js
@@ -0,0 +1,21 @@
+// change the file to [sub].js
+export default {
+    owner: {
+        // your github username
+        user: "pathfindertogami",
+        // your github email
+        email: "otsosdarom@gmail.com",
+    },
+    records: [
+        // can include multiple records, if some of them conflict, may be overwritten
+        {
+            // type of DNS record
+            type: "CNAME",
+            record: "cname.vercel-dns.com.",
+            // using Cloudflare CDN
+            proxied: true,
+            // TTL, (s), must be between 60 and 86400
+            ttl: 60,
+        },
+    ]
+}
