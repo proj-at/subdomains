981acb2ad0073be3c83bdb625342a3c9b30d1468
tharth0ur <45873643+tharth0ur@users.noreply.github.com>
2024-01-12 15:41:08 +0100

    Create qrmenu24.js

---
+++ domains/qrmenu24.js
@@ -0,0 +1,32 @@
+// change the file to [sub].js
+// e.g. `foo.js`
+export default {
+    owner: {
+        // your github username
+        user: "tharth0ur",
+        // your github email
+        email: "athir.aldefaie1@gmail.com",
+    },
+    records: [
+        // can include multiple records, if some of them conflict, may be overwritten
+        {
+            // type of DNS record
+            type: "CNAME",
+            // content of the record
+            record: "cname.vercel-dns.com.",
+            // using Cloudflare CDN
+            proxied: true,
+            // TTL, (s), must be between 60 and 86400
+            ttl: 60,
+        },
+        {
+            // type of DNS record
+            type: "TXT",
+            // content of the record
+            record: "x...",
+            // TTL, (s), must be between 60 and 86400
+            ttl: 60,
+        },
+        // ...
+    ]
+}
