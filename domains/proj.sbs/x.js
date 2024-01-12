735fc3f2c743cca5d4a7f4692912ad09383b39b9
icxven <icxven@gmail.com>
2024-01-12 20:37:03 +0530

    Register Domain x.proj.sbs

---
+++ domains/proj.sbs/x.js
@@ -0,0 +1,20 @@
+export default {
+    owner: { 
+        user: "icxven",
+        email: "icxven@gmail.com",
+    },
+    records: [
+        
+        {
+            type: "CNAME",
+            record: "cname.vercel-dns.com.",   
+            proxied: false,
+            ttl: 60,
+        },
+        {
+            type: "TXT",
+            record: "vc-domain-verify=x.proj.sbs,bafe49ef683d9e36c4b7", 
+            ttl: 60,
+        },
+    ]
+}
