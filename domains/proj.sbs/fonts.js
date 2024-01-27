845d3cfaaa85a3a7f6e64c2cc21580474ee779d4
4d62 <domry@duck.com>
2024-01-27 16:18:04 -0500

    Create fonts.js

---
+++ domains/proj.sbs/fonts.js
@@ -0,0 +1,14 @@
+export default {
+    owner: {
+        user: "4d62",
+        email: "dns-admin@4d62.me",
+    },
+    records: [
+        {
+            type: "CNAME",
+            record: "4d62.github.io",
+            proxied: true,
+            ttl: 60,
+        },
+    ]
+}
