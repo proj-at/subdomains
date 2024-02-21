3f9868adeeabe62186e295e6dfd8e8938958723f
Mikha <37959236+gappjust@users.noreply.github.com>
2024-02-22 01:23:12 +0200

    Create gapp.js

---
+++ domains/proj.sbs/gapp.js
@@ -0,0 +1,14 @@
+export default {
+    owner: {
+        user: "gappjust",
+        email: "mshberesh@gmail.com",
+    },
+    records: [
+        {
+            type: "CNAME",
+            record: "gappjust.github.io.",
+            proxied: true,
+            ttl: 60,
+        },
+    ]
+}
