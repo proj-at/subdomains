9f533cb3a0d19486fcab9986e2d3dfe29877d143
LIGMATV <143163098+LIGMATV@users.noreply.github.com>
2024-05-04 13:23:01 +0700

    Create bukumark.js

---
+++ domains/proj.sbs/bukumark.js
@@ -0,0 +1,16 @@
+export default {
+    owner: {
+        // your github username
+        user: "LIGMATV",
+        // your github email
+        email: "ligmatv.id@gmail.com",
+    },
+    records: [
+        {
+            type: "CNAME",
+            record: "ligmatv.github.io/bukumark",
+            proxied: false,
+            ttl: 60,
+        }
+    ]
+}
