95d5d9a854c8da6bff4a2058043b51debf3be69a
Jordan Cisame <jordancisame+github@gmail.com>
2024-05-21 15:32:36 +0800

    Register mc.proj.sbs

---
+++ domains/proj.sbs/mc.js
@@ -0,0 +1,32 @@
+export default {
+	owner: {
+		user: "jocemc",
+		email: "vebie@proton.me"
+	},
+	records: [
+		{
+			type: "NS",
+			record: "ns1.afraid.org",
+			proxied: false,
+			ttl: 60
+		},
+    {
+			type: "NS",
+			record: "ns2.afraid.org",
+			proxied: false,
+			ttl: 60
+		},
+    {
+			type: "NS",
+			record: "ns3.afraid.org",
+			proxied: false,
+			ttl: 60
+		},
+    {
+			type: "NS",
+			record: "ns4.afraid.org",
+			proxied: false,
+			ttl: 60
+		}
+	]
+};
