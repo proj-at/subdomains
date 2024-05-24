182e3b0d33822273cfac96e42bac3caf68487109
Wekuz <wekuz@duck.com>
2024-05-24 22:21:56 +0300

    Create setuprank.js

---
+++ domains/proj.sbs/setuprank.js
@@ -0,0 +1,14 @@
+export default {
+    owner: {
+        user: "Wekuz",
+        email: "wekuz@duck.com",
+    },
+    records: [
+        {
+            type: "CNAME",
+            record: "setuprank.pages.dev",
+            proxied: true,
+            ttl: 600,
+        },
+    ]
+}
