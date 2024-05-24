bb4c51ae53e25f3cfe209918523ad6e098950ae3
Wekuz <wekuz@duck.com>
2024-05-24 22:25:22 +0300

    Create auth.setuprank.js

---
+++ domains/proj.sbs/auth.setuprank.js
@@ -0,0 +1,14 @@
+export default {
+    owner: {
+        user: "Wekuz",
+        email: "wekuz@duck.com",
+    },
+    records: [
+        {
+            type: "CNAME",
+            record: "pro-4729237707907295471.frontendapi.corbado.io",
+            proxied: true,
+            ttl: 600,
+        },
+    ]
+}
