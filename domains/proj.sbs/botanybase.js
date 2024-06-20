91dadc72823f22b70b0b58956016f01f98fd06f5
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-06-20 18:29:46 +0800

    Create botanybase.js

---
+++ domains/proj.sbs/botanybase.js
@@ -0,0 +1,23 @@
+// change the file to [sub].js
+// e.g. `foo.js`
+export default {
+    owner: {
+        // your github username
+        user: "melonmasteristaken",
+        // your github email
+        email: "github@proj.at",
+    },
+    records: [
+        // can include multiple records, if some of them conflict, may be overwritten
+        {
+            // type of DNS record
+            type: "CNAME",
+            // content of the record
+            record: "botanybase.github.io.",
+            // using Cloudflare CDN
+            proxied: true,
+            // TTL, (s), must be between 60 and 86400
+            ttl: 60,
+        }
+    ]
+}
